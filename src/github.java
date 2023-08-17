9daea208b1e69914fead321a19627981cf04ea8c
astroalexa <naunghtoomya774@gmail.com>
2023-08-17 09:24:23 +0630

    version1

---
+++ src/github.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class github {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			JOptionPane.showMessageDialog(null,"Hello IMU");
+			JOptionPane.showMessageDialog(null, "Hello Batch 97");
+		
+	}
+
+}
